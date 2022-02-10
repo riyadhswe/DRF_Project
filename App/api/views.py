2b6f39c4cdac52fb5dbcc14db407afe5d848da2b
riyadhswe <riyadhswe@gmail.com>
2022-02-10 21:28:34 +0600

    Serializer Fields and Core Arguments

--- App/api/views.py
+++ App/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import *
+from rest_framework import status
 from App.models import *
 from rest_framework.decorators import api_view
 from App.api.serializers import *
@@ -17,13 +18,16 @@ def movie_list(request):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET','PUT', 'DELETE'])
 def movie_details(request, pk):
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'Error': 'Movie Not Found'},status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
 
@@ -33,11 +37,11 @@ def movie_details(request, pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
     if request.method == 'DELETE':
         movie = Movie.objects.get(pk=pk)
         movie.delete()
-        return Response()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
